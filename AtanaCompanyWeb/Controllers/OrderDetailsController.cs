693807fbdce2ef6c381531c3664cfa6f98fedb87
Atana <nikolaatanaskovic92@gmail.com>
2024-01-18 10:14:15 +0100

    OrderDetail tab customization

--- AtanaCompanyWeb/Controllers/OrderDetailsController.cs
+++ AtanaCompanyWeb/Controllers/OrderDetailsController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using AtanaCompanyWeb.Models;
 using Microsoft.AspNetCore.Authorization;
+using ClosedXML.Excel;
 
 namespace AtanaCompanyWeb.Controllers
 {
@@ -21,39 +22,72 @@ namespace AtanaCompanyWeb.Controllers
 
         // GET: OrderDetails
         [Authorize(Roles = "Administrator, User")]
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(int? pageNumber, string searchString)
         {
-            var tEST_DOOContext = _context.OrderDetails.Include(o => o.Order).Include(o => o.Product);
-            return View(await tEST_DOOContext.ToListAsync());
-        }
+            int pageSize = 25;
 
-        // GET: OrderDetails/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null || _context.OrderDetails == null)
+            var tEST_DOOContext = _context.OrderDetails.Include(o => o.Order).Include(o => o.Product).AsQueryable();
+
+            if (!String.IsNullOrEmpty(searchString))
             {
-                return NotFound();
+
+                int prefixLength = searchString.ToString().Length;
+
+                tEST_DOOContext = tEST_DOOContext.Where(o => o.Orderid.ToString().StartsWith(searchString.ToString().Substring(0, prefixLength)));
             }
 
-            var orderDetail = await _context.OrderDetails
-                .Include(o => o.Order)
-                .Include(o => o.Product)
-                .FirstOrDefaultAsync(m => m.Orderid == id);
-            if (orderDetail == null)
-            {
-                return NotFound();
-            }
+            tEST_DOOContext = tEST_DOOContext.OrderByDescending(o => o.Orderid);
 
-            return View(orderDetail);
+            var paginatedList = PaginatedList<OrderDetail>.Create(await tEST_DOOContext.ToListAsync(), pageNumber ?? 1, pageSize);
+
+            ViewData["SearchString"] = searchString;
+
+            return View(paginatedList);
+
         }
 
-        // GET: OrderDetails/Create
-        public IActionResult Create()
+        public IActionResult ExcelExport()
         {
-            ViewData["Orderid"] = new SelectList(_context.Orders, "Orderid", "Orderid");
-            ViewData["Productid"] = new SelectList(_context.Products, "Productid", "Productid");
-            return View();
+            using (var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add("Orders");
+                var currentRow = 1;
+
+                #region Header
+                worksheet.Cell(currentRow, 1).Value = "Order ID";
+                worksheet.Cell(currentRow, 2).Value = "Unitprice";
+                worksheet.Cell(currentRow, 3).Value = "Qty";
+                worksheet.Cell(currentRow, 4).Value = "Discount";
+                worksheet.Cell(currentRow, 5).Value = "Product";
+                #endregion
+
+                #region Body
+
+                foreach (var orderDetails in _context.OrderDetails)
+                {
+                    currentRow++;
+                    worksheet.Cell(currentRow, 1).Value = orderDetails.Orderid;
+                    worksheet.Cell(currentRow, 2).Value = orderDetails.Unitprice;
+                    worksheet.Cell(currentRow, 3).Value = orderDetails.Qty;
+                    worksheet.Cell(currentRow, 4).Value = orderDetails.Discount;
+                    worksheet.Cell(currentRow, 5).Value = orderDetails.Productid;
+                }
+                #endregion
+
+                using (var stream = new MemoryStream())
+                {
+                    workbook.SaveAs(stream);
+                    var content = stream.ToArray();
+
+                    return File(
+                        content,
+                        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                        "OrderDetails.xlsx"
+                        );
+                }
+            }
         }
+
 
         // POST: OrderDetails/Create
 
@@ -84,25 +118,35 @@ namespace AtanaCompanyWeb.Controllers
                 return NotFound();
             }
 
-            var orderDetail = await _context.OrderDetails.FindAsync(id);
+            //var orderDetail = await _context.OrderDetails.FindAsync(id);
+
+            var orderDetail = await _context.OrderDetails
+            .Where(od => od.Orderid == id) 
+            .FirstOrDefaultAsync();
+
             if (orderDetail == null)
             {
                 return NotFound();
             }
             ViewData["Orderid"] = new SelectList(_context.Orders, "Orderid", "Orderid", orderDetail.Orderid);
             ViewData["Productid"] = new SelectList(_context.Products, "Productid", "Productid", orderDetail.Productid);
+
             return View(orderDetail);
         }
-        [Authorize(Roles = "Administrator")]
+        
         // POST: OrderDetails/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Administrator")]
         public async Task<IActionResult> Edit(int id, [Bind("Orderid,Productid,Unitprice,Qty,Discount")] OrderDetail orderDetail)
         {
             if (id != orderDetail.Orderid)
             {
                 return NotFound();
             }
+
+            ModelState.Remove("Order");
+            ModelState.Remove("Product");
 
             if (ModelState.IsValid)
             {
--- AtanaCompanyWeb/Models/OrderDetail.cs
+++ AtanaCompanyWeb/Models/OrderDetail.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace AtanaCompanyWeb.Models
 {
     public partial class OrderDetail
     {
+        
         public int Orderid { get; set; }
         public int Productid { get; set; }
+        [Range(0, 1000000, ErrorMessage = "The Price can be between 0 and 1M !")] //za ogranicenje
         public decimal Unitprice { get; set; }
+        [Range(0, 1000000, ErrorMessage = "The Qty can be between 0 and 1M !")] //za ogranicenje
         public short Qty { get; set; }
+        [Range(0.0, 1.0, ErrorMessage = "The Discount must be between 0 and 1.")]
         public decimal Discount { get; set; }
 
         public virtual Order Order { get; set; } = null!;
